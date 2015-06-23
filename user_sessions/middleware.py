c29ed29888eea570f99ffcfa74bd032ca5a324a3
kyle-eshares <kyle@esharesinc.com>
2015-06-23 12:55:12 -0700

    Update middleware.py
    Suppress the `RemovedInDjango19Warning`

--- user_sessions/middleware.py
+++ user_sessions/middleware.py
@@ -3,7 +3,7 @@ import time
 from django.conf import settings
 from django.utils.cache import patch_vary_headers
 from django.utils.http import cookie_date
-from django.utils.importlib import import_module
+from importlib import import_module
 
 
 class SessionMiddleware(object):
